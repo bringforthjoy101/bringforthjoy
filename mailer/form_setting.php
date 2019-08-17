119fa9fb453b9b64b0ba0a76baffc05527904d3f
bringforthjoy101 <54186498+bringforthjoy101@users.noreply.github.com>
2019-08-17 23:57:29 +0100

    Add files via upload

---
+++ mailer/form_setting.php
@@ -0,0 +1,8 @@
+<?php
+	/*Form settings*/
+	$subj = "New message from My site 'Bringforthjoy'"; //letter subject
+	$to = 'adelugba.emma@gmail.com'; // Enter Your E-mail
+	$from = 'adelugba.emma@gmail.com'; // Admin e-mail
+	$fromName = 'Adelugba Emmanuel (Bringforthjoy)'; // Your company name
+	$charset = 'UTF-8';
+?>
